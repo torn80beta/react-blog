af02e7fb9692df4d7e44f9207b69836c6fa29b4a
Alex <103459735+torn80beta@users.noreply.github.com>
2023-05-30 00:55:50 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from 'pages/Home/Home';
+import { About } from 'pages/About/About';
+import { Products } from 'pages/Products/Products';
+import { NotFound } from 'pages/NotFound/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="about" end>
+          About
+        </NavLink>
+        <NavLink to="products" end>
+          Products
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
